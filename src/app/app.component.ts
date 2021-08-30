b50981c72373d746aedb85c927f5e62dcba96149
Tony Uifalean <tonyuifalean@gmail.com>
2021-08-30 20:08:43 +0300

    Change application title.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'sighisoara-festival';
+  title = 'Sighisoara Festival';
 }
