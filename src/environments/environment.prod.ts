ca31f5cb939c1c7ad6a291c292c6cbc25328b71a
Tony Uifalean <tonyuifalean@gmail.com>
2021-10-12 22:50:00 +0300

    #SF14: Remove Firebase Integration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,12 +1,3 @@
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyAEXM22rf6WxlDijGgKItHVo2iDlfaaL8M",
-    authDomain: "sighisoarafestival-b5038.firebaseapp.com",
-    projectId: "sighisoarafestival-b5038",
-    storageBucket: "sighisoarafestival-b5038.appspot.com",
-    messagingSenderId: "897805129438",
-    appId: "1:897805129438:web:86ef7976d41fd462f1944f",
-    measurementId: "G-639M41BHXK"
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,16 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
-  firebase: {
-    apiKey: "AIzaSyAEXM22rf6WxlDijGgKItHVo2iDlfaaL8M",
-    authDomain: "sighisoarafestival-b5038.firebaseapp.com",
-    projectId: "sighisoarafestival-b5038",
-    storageBucket: "sighisoarafestival-b5038.appspot.com",
-    messagingSenderId: "897805129438",
-    appId: "1:897805129438:web:86ef7976d41fd462f1944f",
-    measurementId: "G-639M41BHXK"
-  }
+  production: false
 };
 
 /*
