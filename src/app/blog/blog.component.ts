a9e04549f7d11c2e794d4c2b7d62cc3141b0552c
Tony Uifalean <tonyuifalean@gmail.com>
2022-03-11 22:09:27 +0400

    #SF30: Create blog post component

--- src/app/blog/blog.component.ts
+++ src/app/blog/blog.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BlogPost } from './subcomponents/blog-post/blog-post.component';
+
 @Component({
   selector: 'app-blog',
   templateUrl: './blog.component.html',
@@ -7,10 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BlogComponent implements OnInit {
   panelOpenState = false;
+  isLoading = true;
 
-  constructor() {}
+  /** Represents the list of blog posts with their number of paragraphs. */
+  blogPostList: BlogPost[] = [{
+    imageData: {
+      url: "assets/images/sighisoara-sunset.jpeg",
+      title: "Sighisoara Sunset"
+    },
+    noOfParagraphs: 8
+  }];
 
-  ngOnInit(): void {
+  constructor() { }
+
+  ngOnInit() {
+    setTimeout(() => {
+      this.isLoading = false;
+    }, 1000);
   }
-
 }
--- src/app/blog/blog.module.ts
+++ src/app/blog/blog.module.ts
@@ -2,20 +2,26 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { MatExpansionModule } from '@angular/material/expansion';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { TranslateModule } from '@ngx-translate/core';
 
+import { SpinnerComponent } from '@app/components/spinner/spinner.component';
 import { httpTranslateLoader } from '../app.module';
 import { BlogComponent } from './blog.component';
 import { BlogRoutingModule } from './blog-routing.module';
+import { BlogPostComponent } from './subcomponents/blog-post/blog-post.component';
 
 @NgModule({
   declarations: [
-    BlogComponent
+    BlogComponent,
+    BlogPostComponent,
+    SpinnerComponent
   ],
   imports: [
     CommonModule,
     BlogRoutingModule,
     MatExpansionModule,
+    MatProgressSpinnerModule,
     TranslateModule.forChild({
       loader: {
         provide: TranslateModule,
